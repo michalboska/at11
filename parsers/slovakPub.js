ab0b9de80ad056f0b21f0613b088b8601f367c97
Michal Boska <michal.boska@mimacom.com>
2015-10-12 10:40:33 +0200

    Finished parser for Slovak Pub

--- parsers/slovakPub.js
+++ parsers/slovakPub.js
@@ -2,7 +2,39 @@ var cheerio = require('cheerio');
 var parserUtil = require('./parserUtil');
 var request = require('request');
 var moment = require('moment-timezone');
+var _ = require('lodash');
 
 module.exports.parse = function(html, date, callback) {
-    throw new Error('Not implemented yet');
+    const DATE_REGEX = /(\d{1,2}) ?\. ?(\d{1,2})\.? ?/i;
+
+    var $ = cheerio.load(html);
+
+    //Meals are quite unstructured, so we need to load everything div-by-div and implement a simple "state machine"
+    //we simply parse everything, ignoring entries that are for different dates than today
+    var rootElem = $('.item-page');
+    var day = undefined,
+        month = undefined,
+        isFirstLine = false,
+        meals = [];
+    var todayDay = moment().format('D'),
+        todayMonth = moment().format('M');
+
+    rootElem.children('p').each(function () {
+        var elem = $(this).text().trim();
+        if (DATE_REGEX.test(elem)) { //a new section for a new date is starting
+            var dateParsed = DATE_REGEX.exec(elem);
+            day = dateParsed[1];
+            month = dateParsed[2];
+            isFirstLine = true; //next line will be the first meal (soup) for this date
+        } else if (day === todayDay && month === todayMonth) { //we only want today's meals
+            meals.push({
+                isSoup: isFirstLine,
+                text: elem,
+                price: 0
+            });
+            isFirstLine = false;
+        }
+    });
+
+    callback(meals);
 };
