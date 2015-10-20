8416795c6f46b5c951b2b1fcb870ca6ea6249336
Michal Boska <michal.boska@mimacom.com>
2015-10-20 11:45:54 +0200

    Start developing parser for Green Tree

---
+++ parsers/greenTree.js
@@ -0,0 +1,42 @@
+var cheerio = require('cheerio');
+var parserUtil = require('./parserUtil');
+var _ = require('lodash');
+
+module.exports.parse = function(html, date, callback) {
+
+    const DATE_REGEX = /(\d{1,2})\. ?(\d{1,2})\. ?(\d{4})/i;
+    const ALERGENS_REGEX = /\(?[\d\,\-]+?\)?/ig;
+    const IGNORE_PATTERN = 'obilniny';
+    const SOUP = 'polievka';
+
+    var $ = cheerio.load(html);
+
+    var todaysMenuElem = $('.dnesne_menu');
+    var dateText = todaysMenuElem.find('h2').text();
+
+    var dateMatch = DATE_REGEX.exec(dateText);
+    if (!dateMatch) {
+        throw new Error('Could not find section for the specified date');
+    }
+
+    if ((date.format('DD') != dateMatch[1]) || (date.format('MM') != dateMatch[2]) || (date.format('YYYY') != dateMatch[3])) {
+        throw new Error('The today\'s section is for different date');
+    }
+
+    var meals = [];
+    todaysMenuElem.find('.jedlo_polozka').each(function () {
+        var foodElem = $(this);
+        var foodName = foodElem.find('.left').text();
+        var foodPrice = foodElem.find('.right').text();
+        if (foodName.indexOf(IGNORE_PATTERN) > -1) {
+            return true;
+        }
+        meals.push({
+            isSoup: foodName.toLowerCase().indexOf(SOUP) > -1,
+            text: foodName.normalizeWhitespace().replace(ALERGENS_REGEX, ''),
+            price: parserUtil.parsePrice(foodPrice).price
+        });
+    });
+
+    callback(meals);
+};
