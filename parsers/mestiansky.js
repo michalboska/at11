799b85dab3d65fbbcf915a86079ab9fc856ec43c
Michal Boska <michal.boska@mimacom.com>
2016-07-28 10:30:07 +0200

    Remove Mestiansky

--- parsers/mestiansky.js
+++
@@ -1,9 +0,0 @@
-var cheerio = require('cheerio');
-var parserUtil = require('./parserUtil');
-var _ = require('lodash');
-
-module.exports.parse = function(html, date, callback) {
-
-
-    callback();
-};
